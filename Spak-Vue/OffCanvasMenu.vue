4b3de33027d1a4140b480c8fd11595b936ac0619
AB <gaborediv.dasedopo@jollyfree.com>
2023-03-23 12:17:23 +0100

    Modifiche maggiori, cambio versione v2.1.3
    variables.scss 
    - rimosso variabili inutili + creazione var css
    - aggiunto var css per componenti spak-vue
    Spak-vue
    - modifiche a offcanvas + dropdown
    form.scss
    - modifiche minori

--- Spak-Vue/OffCanvasMenu.vue
+++ Spak-Vue/OffCanvasMenu.vue
@@ -1,27 +1,41 @@
 <script setup>
-import { ref, onMounted } from "vue";
+//import { ref, onMounted } from "vue";
+
+const props = defineProps({
+  rightEntrance: Boolean, //true, false
+  maxWidth: String, //100%
+});
+
+const emit = defineEmits(["after-open", "after-close"]);
 
 const offCanvasMenu = ref(null);
 const isShow = ref(false);
 
 function open() {
-  offCanvasMenu.value.style.width = "100%";
   isShow.value = true;
-  bodyOverflow(isShow.value);
-  offCanvasMenu.value.ontransitionend;
+  emit("after-open");
 }
 function close() {
-  offCanvasMenu.value.style.width = "0%";
   isShow.value = false;
-  bodyOverflow(isShow.value);
+  emit("after-close");
 }
 
-function bodyOverflow(isShow) {
-  document.querySelector("body").style.overflow = isShow ? "hidden" : "";
-}
-
-onMounted(() => {
-  bodyOverflow(isShow.value);
+const enterClass = ref(
+  `animate__animated ${
+    props.rightEntrance ? "animate__slideInRight" : "animate__slideInLeft"
+  }`
+);
+const leaveClass = ref(
+  `animate__animated ${
+    props.rightEntrance ? "animate__slideOutRight" : "animate__slideOutLeft"
+  }`
+);
+const overlayStyle = computed(() => {
+  let st = {
+    width: props.maxWidth,
+  };
+  props.rightEntrance ? (st.right = 0) : (st.left = 0);
+  return st;
 });
 
 defineExpose({
@@ -32,72 +46,99 @@ defineExpose({
 </script>
 
 <template>
-  <div id="offCanvasMenu" ref="offCanvasMenu" class="overlay">
-    <div class="closebtn"><i class="bi bi-x" @click="close"></i></div>
-    <div class="overlay-body">
-      <div class="overlay-content">
-        <slot name="content"></slot>
+  <Transition>
+    <div
+      class="offcanvas-bg"
+      v-if="isShow"
+      :class="{ 'is-right': props.rightEntrance }"
+    ></div>
+  </Transition>
+  <Transition
+    name="custom-class"
+    :enter-active-class="enterClass"
+    :leave-active-class="leaveClass"
+  >
+    <div
+      v-if="isShow"
+      id="offCanvasMenu"
+      ref="offCanvasMenu"
+      class="overlay"
+      :style="overlayStyle"
+      :class="{ 'is-right': props.rightEntrance }"
+    >
+      <div class="overlay-header">
+        <span>
+          <slot name="header"></slot>
+        </span>
+        <span class="closebtn">
+          <i class="bi bi-x" @click="close"></i>
+          <i class="fa-solid fa-xmark" @click="close"></i>
+        </span>
       </div>
-      <div class="overlay-footer">
-        <slot name="footer"></slot>
+      <div class="overlay-body">
+        <div class="overlay-content">
+          <slot name="content"></slot>
+        </div>
+        <div class="overlay-footer">
+          <slot name="footer"></slot>
+        </div>
       </div>
     </div>
-  </div>
+  </Transition>
 </template>
 
 <style scoped lang="scss">
-@import "@/assets/Spak-scss/colors";
 .overlay {
-  //height: 100%;
-  //max-height: 100vh;
-  width: 0;
+  width: 100%;
   position: fixed;
   display: block;
-  z-index: 1;
-  left: 0;
+  z-index: 1001;
   top: 0;
   bottom: 0;
-  background-color: $secondary;
-  background-color: rgba($secondary, 0.9);
+  background-color: var(--spak-comp-offcanvas-bg-color);
   overflow-x: hidden;
   transition: 0.5s;
-  color: lighten($primary-text, 20%);
-  //padding-bottom: 5em;
+  color: var(--spak-comp-offcanvas-color);
+  padding: 0.5em 1.3em;
+}
+.overlay-header {
+  color: var(--spak-comp-offcanvas-color);
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 }
 .closebtn {
   font-size: 2.2rem;
-  text-align: right;
-  padding: 0.5em 0.5em 0 0;
-  color: lighten($primary-text, 20%);
   i {
     cursor: pointer;
   }
 }
 
-.overlay-body {
-  display: flex;
-  flex-flow: column nowrap;
+.offcanvas-bg {
+  z-index: 1000;
+  display: block;
   height: 100%;
-  min-height: 100%;
-  justify-content: space-between;
-  padding: 0 0 20% 0;
-}
-
-.overlay-content {
-  margin: 1.5em 1em;
-  text-align: center;
-  display: flex;
-  flex-flow: column nowrap;
-  justify-content: flex-start;
-  //align-items: center;
-  gap: 1.7em;
-}
-
-.overlay-footer {
-  display: flex;
-  flex-flow: column nowrap;
-  align-items: center;
-  justify-content: center;
-  margin: 2.5em 1em;
+  width: 100%;
+  position: fixed;
+  top: 0;
+  left: 0;
+  background-color: var(--spak-comp-offcanvas-background-bg-color);
+  overflow: auto;
+  padding: 2em 0em;
+  -ms-overflow-style: none;
+  scrollbar-width: none;
+  &::-webkit-scrollbar {
+    display: none;
+  }
 }
 </style>
+
+<!-- 
+  -- necessario animate.css
+  -- props 
+  --  -> rightEnter: Boolean, -> serve per sapere da dove entrata offcanvas
+  --  -> maxWidth: String, (ex. '100%') -> massima apertura offcanvas
+  -- slot
+  --  -> header, content, footer
+  -- emits
+ -->
